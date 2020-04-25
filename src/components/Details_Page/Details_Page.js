13cc8d05a516c3cbd1716a9b2c51ef640f23e259
Caleb Wistuba <calebwistuba@gmail.com>
2020-04-25 18:15:06 -0500

    rendered the title and description to the dom

--- src/components/Details_Page/Details_Page.js
+++ src/components/Details_Page/Details_Page.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Details_Page extends Component {
+  componentDidMount() {
+    this.props.dispatch({ type: "GET_MOVIES" });
+  }
   clickBack = (event) => {
     this.props.history.push(`/`);
   };
@@ -10,13 +14,32 @@ class Details_Page extends Component {
   };
 
   render() {
+    const id = Number(this.props.match.params.id);
+    console.log("ID", id);
+
+    const filteredMovie = this.props.store.movies.filter((movie) => {
+      return movie.id === id;
+    });
+    console.log("filteredMovies:", filteredMovie);
+
     return (
       <div>
         <button onClick={this.clickBack}>Back</button>
         <button onClick={this.clickEdit}>Edit</button>
+        <div>
+          {filteredMovie.map((movie) => {
+            return (
+              <div key={movie.id}>
+                <h2>{movie.title}</h2>
+                <p>{movie.description}</p>
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
 }
 
-export default Details_Page;
+const mapStoreToProps = (store) => ({ store });
+export default connect(mapStoreToProps)(Details_Page);
